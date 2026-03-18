0f2bc0194fc69606c80a12d778f3d20dbd7dbea1
Deepak <deepakbindal323@gmail.com>
2026-03-18 18:18:39 +0530

    Fixed appsettings json

--- Shared.Contracts/obj/Debug/net8.0/Shared.Contracts.AssemblyInfo.cs
+++ Shared.Contracts/obj/Debug/net8.0/Shared.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c182574e9df7509226c1b873eae32442d18c4e89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ea87f3e5a29763dd56cced6eb5d75e09fedb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
